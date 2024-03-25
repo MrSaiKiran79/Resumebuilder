a3325e717ca30e41766e1f70f08993120c0e2f78
SaiKiran <137259511+MrSaiKiran79@users.noreply.github.com>
2024-03-25 13:15:32 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,72 @@
+const profileData = {
+    title: "Resume",
+    name: "Kondoju SaiKiran",
+    sub_title: "Frontend Engineer",
+    logoURL: "assets/images/dp.jpg",
+    about: {
+      intro: `I am an innovative and passionate developer with a primary interest in <strong>JavaScript</strong>
+       and its applications. Ready to start my carrer in designing and implementing sophisticated UI/UX with
+        Progressive Web Application standards and application life-cycle management.`,
+      contact: {
+        email: "saikirankondoju78@gmail.com",
+        phone: "+91-7981544362",
+        address: "Hyderbad,Telangana,India"
+      }
+    },
+    links: [
+      //{ title: "Codepen", src: "https://codepen.io/imvpn22" },
+      { title: "Github", src: "https://github.com/mrkiran792" },
+      { title: "LinkedIn", src: "https://www.linkedin.com/in/Mrkiran79" }
+      // { title: 'Twitter', src: 'https://twitter.com/imvpn22' }
+      // { title: 'HackerEarth', src: 'https://www.hackerearth.com/@imvpn22' },
+    ],
+    education: [
+      {
+        alma:"SPHN,Hyderbad",
+        duration: "2020 - 2024",
+        std: "B.Tech. (Computer Science & Engineering)",
+        score: "70.00%"
+      },
+      {
+        alma: "Narayana Junior College,Hyderbad",
+        duration: "2018 - 2020",
+        std: "Intermediate",
+        score: "75.00%"
+      },
+      {
+        alma: "SGHS,Hyderbad",
+        duration: "2012 - 2018",
+        std: "Class VI - X",
+        score: "88.00%"
+      }
+    ],
+    skills: [
+      {
+        category: "Programming Languages",
+        topics: ["Python", "Java"]
+      },
+      {
+        category: "Other Skills",
+        topics:["Leadership","Time Management","Communication","Problem Solving"]
+      },
+    ],
+    projects: [
+      {
+        title: "Phishing Link Detector using Machine learning Algorithms",
+        duration: "June - Nov 2024",
+        link: "",
+        desc: `A web app to Check wheather the given link is Phishing or not.`
+      },
+    ],
+    experiences: [
+    ],
+    certifications: [
+      {
+        desc: `<strong>Python Certification</strong> by <strong>Hackerrank and codetanta</strong>.`,
+      
+      },
+      {
+        desc:`CyberYoddha Certification By Rachakonda Security Council`,
+      },
+    ]
+  };
